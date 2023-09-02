411dab48ffe823a943ee4429ff2dd02e08a5909b
Lucas Reis <108764828+LucasRe1s@users.noreply.github.com>
2023-09-02 16:31:44 -0300

    feat: estudo de variaveis e metodos. e um mini projeto simulando uma smart tv.

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,9 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        // short numeroCurto2 = (short) numeroNormal;
+
+        // final double VALOR_DE_PI = 3.14;
+    }
+}
